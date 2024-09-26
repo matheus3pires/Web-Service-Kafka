b2361abb0a7df94478fbd2fc7ae974139ad4fdac
Matheus <matheuspiressdev@gmail.com>
2024-09-26 15:59:23 -0300

    First Commit

---
+++ src/main/java/atividade02/com/br/werbservicekafka/WerbServiceKafkaApplication.java
@@ -0,0 +1,13 @@
+package atividade02.com.br.werbservicekafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WerbServiceKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WerbServiceKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/atividade02/com/br/werbservicekafka/WerbServiceKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package atividade02.com.br.werbservicekafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WerbServiceKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
